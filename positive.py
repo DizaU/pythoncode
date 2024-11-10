b875d8979dd9c3573007796c9eab31a54a637f92
DizaU <9365@sanskritischool.edu.in>
2024-11-10 19:01:38 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,8 @@
+number=int(input("Enter a number:"))
+if(number>0):
+            print("positive")
+else:
+             if(number==0):
+                            print("0")
+              else:
+                            print("negative")
