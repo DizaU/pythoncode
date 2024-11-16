bbfd5fd35c63ffe7ce440e4ff9307808e4c50cf4
DizaU <9365@sanskritischool.edu.in>
2024-11-16 22:16:31 +0530

    Create Grade.py

---
+++ Grade.py
@@ -0,0 +1,14 @@
+grade=int(input("Enter your grade:"))
+if(grade>=90):
+      print("A+ grade")
+else:
+      if(grade>=80):
+                     print("A grade")
+       else:
+                     if(grade>=60):
+                                    print("B grade")
+                      else:
+                                    if(grade>=50):
+                                              print("C grade")
+                                     else::
+                                               print("Fail")
