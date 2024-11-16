b8be444cca7c8eb9f2449141e6a915264c338bb4
DizaU <9365@sanskritischool.edu.in>
2024-11-16 22:29:42 +0530

    Update Grade.py

--- Grade.py
+++ Grade.py
@@ -1,14 +1,11 @@
 grade=int(input("Enter your grade:"))
 if(grade>=90):
       print("A+ grade")
+elif(grade>=80):
+      print("A grade")
+elif(grade>=60):
+      print("B grade")
+elif(grade>=50):
+      print("C grade")
 else:
-      if(grade>=80):
-                     print("A grade")
-       else:
-                     if(grade>=60):
-                                    print("B grade")
-                      else:
-                                    if(grade>=50):
-                                              print("C grade")
-                                     else::
-                                               print("Fail")
+      print("Fail")
