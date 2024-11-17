41c547e8f9702f185f15233607ba1e2eec194f7f
DizaU <9365@sanskritischool.edu.in>
2024-11-17 11:01:14 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -20,7 +20,7 @@ elif(rating==1):
 elif(rating>5 or rating<0):
 	print("invalid rating")
 	validRating=False
-if (validRating=True):    #then better way of writing it is if(validrating)
+if (validRating==True):    #better way of writing it is if(validRating)
 	print("Bonus in rupees is",bonus)
 	print("So your total earnings is rupees",salary+bonus)
 
