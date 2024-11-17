094be56eca488c5827f8bffc0e6bfbcee6e11caf
DizaU <9365@sanskritischool.edu.in>
2024-11-17 19:14:27 +0530

    Create Program3.py

---
+++ Program3.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+if(a>b):
+	print("First number is greater than the second")
+elif(a==b):
+	print("They both are equal")
+else:
+	print("First number is lesser than the second")
